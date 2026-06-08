36f22a881ee931a0ea978ec6f53705fa06c74156
Harshada Rahate <harshadarahate20@gmail.com>
2026-06-08 23:49:21 +0530

    Added Supplier Controller and GET API

--- Backened/src/main/java/com/inventory/backend/controller/SupplierController.java
+++ Backened/src/main/java/com/inventory/backend/controller/SupplierController.java
@@ -1,10 +1,27 @@
 package com.inventory.backend.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.inventory.backend.entity.Supplier;
+import com.inventory.backend.service.SupplierService;
 
 @RestController
 @RequestMapping("/api/v1/suppliers")
 public class SupplierController {
+ private final SupplierService service;
+
+    public SupplierController(SupplierService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Supplier> getAllSuppliers() {
+        return service.getAllSuppliers();
+
  
+}
 }
--- Backened/src/main/java/com/inventory/backend/repository/SupplierRepository.java
+++ Backened/src/main/java/com/inventory/backend/repository/SupplierRepository.java
@@ -1,10 +1,13 @@
 package com.inventory.backend.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class SupplierRepository extends jpaRepository <Supplier,Long>{
+import com.inventory.backend.entity.Supplier;
 
-    
+
+
+public interface SupplierRepository
+        extends JpaRepository<Supplier, Long>
+    {
 
 
 
